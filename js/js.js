e6b1029001d012c9290aec34725daa3d166ed797
alswjd1125 <alslal1125@naver.com>
2020-10-08 14:13:57 +0900

    Galaxy
    Galaxy

--- js/js.js
+++ js/js.js
@@ -14,4 +14,20 @@ $(function(){
         var offset = $("#section04").offset();
         $("html,body").animate({scrollTop : offset.top}, 800);
     }); 
+    $(".more-text").click(function(){
+        var offset = $("#section02").offset();
+        $("html,body").animate({scrollTop : offset.top}, 800);
+    }); 
 });
+
+// 탑 이동 키
+window.onscroll = function() {scrollFunction()};
+    
+function scrollFunction() {
+var $element = matchMedia("screen and (min-width: 414px)").matches ? document.getElementById("scrollTopBtn") : document.getElementById("scrollTopBtnMob");
+$element.style.display = (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) ? 'block' : 'none';
+}
+
+function topFunction() {
+    $('html,body').animate({ scrollTop: 0 }, 500);
+}
