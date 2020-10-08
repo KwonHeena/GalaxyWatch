47f17030b6fc1c7b6412a456d4d5e8b992e0b5ea
alswjd1125 <alslal1125@naver.com>
2020-10-08 13:13:59 +0900

    Galaxy
    Galaxy

--- js/js.js
+++ js/js.js
@@ -1,7 +1,17 @@
 AOS.init();
 
-$('.pink41').mouseover(function(){
-    $(this).on().animate({background:'#e9cccf'}, 500); },
-function(){
-    $(this).stop().animate({background:'#000'}, 2500);
+/**마우스 부드럽게 해당위치로 이동**/
+$(function(){
+    $(".menu01").click(function(){
+    var offset = $("#section01").offset();
+    $("html,body").animate({scrollTop : offset.top}, 800);
+    });
+    $(".menu02").click(function(){
+        var offset = $("#section02").offset();
+        $("html,body").animate({scrollTop : offset.top}, 800);
+    });
+    $(".menu03").click(function(){
+        var offset = $("#section04").offset();
+        $("html,body").animate({scrollTop : offset.top}, 800);
+    }); 
 });
